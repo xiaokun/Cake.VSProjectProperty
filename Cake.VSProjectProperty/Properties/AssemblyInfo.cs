aebb596e85948d3f52fbb59fd98f633fa130d036
fuxiaokun <fxk49@126.com>
2021-01-25 15:49:03 +0800

    .NetStandard 2.0 Cake.Core 1.0.0-rc

--- Cake.VSProjectProperty/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyTitle("Cake.VSProjectProperty")]
-[assembly: AssemblyProduct("Cake.AssemblyInfoReflector")]
-[assembly: AssemblyCopyright("©")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-
